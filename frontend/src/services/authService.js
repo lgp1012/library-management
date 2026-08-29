7e5f375eeb4955037c766b314283704c69376335
lgp1012 <luugiaphuc1012@gmail.com>
2026-08-29 16:44:53 +0700

    refactor(auth): change api endpoint

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,11 +2,19 @@ import api from "../libs/axios";
 
 const authService = {
   signin: async (formData) => {
-    const response = await api.post("/v1/auth/signin", formData);
+    const response = await api.post("/v1/auth/token", formData);
     return response.data;
   },
   signup: async (formData) => {
-    const response = await api.post("/v1/auth/signup", formData);
+    const response = await api.post("/v1/users", formData);
+    return response.data;
+  },
+  signout: async () => {
+    const response = await api.post("/v1/auth/signout");
+    return response.data;
+  },
+  refreshToken: async (refreshToken) => {
+    const response = await api.post("/v1/auth/refresh", { refreshToken });
     return response.data;
   },
 };
